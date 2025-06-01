fe60944e824acd55a22a8edb911ea40d8feb6fe6
Gustavo Petruzzi <gustavopetruzzi19@gmail.com>
2025-06-01 18:42:08 -0300

    First commit

---
+++ littlelemon/restaurant/views.py
@@ -0,0 +1,29 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateAPIView, DestroyAPIView
+from .models import Menu, Booking
+from .serializers import MenuSerializer, BookingSerializer
+
+# Create your views here.
+
+def sayHello(request):
+    return HttpResponse('Hello world')
+
+# Create your views here
+def index(request):
+    return render(request, 'index.html', {})
+
+class MenuItemView(ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class SingleMenuItemView(RetrieveUpdateAPIView, DestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class BookingViewSet():
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+
+
+
